4d6668a477542947d1d6e59b1a28fd04afae38ba
Juan David Rengifo Castro <jdrengifoc@eafit.edu.co>
2025-03-11 20:37:57 -0500

    draft: test dplyr

--- experiment_1/main_template.R
+++ experiment_1/main_template.R
@@ -1,5 +1,6 @@
+library(dplyr)
 param1 <- __param1__
 param2 <- __param2__
 a <- __a__
 
-print(paste("Ejecutando con param1 =", param1, ", param2 =", param2, " y a =", a))
+paste("Ejecutando con param1 =", param1, ", param2 =", param2, " y a =", a) %>% print()
