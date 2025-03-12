bf272f161ae8e93ca3bd4e9646aef54446bfc82f
Juan David Rengifo Castro <jdrengifoc@eafit.edu.co>
2025-03-11 20:26:26 -0500

    draft: try

--- experiment_1/main_template.R
+++ experiment_1/main_template.R
@@ -2,4 +2,4 @@ param1 <- __param1__
 param2 <- __param2__
 a <- __a__
 
-print(paste("Ejecutando con param1 =", param1, ", param2 =", param2" y a =", a))
+print(paste("Ejecutando con param1 =", param1, ", param2 =", param2, " y a =", a))
