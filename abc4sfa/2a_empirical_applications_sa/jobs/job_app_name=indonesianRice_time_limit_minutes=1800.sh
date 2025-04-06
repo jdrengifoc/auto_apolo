602505d9e3f6f19fe547775cf385658ccec99496
Juan - Apolo <jdrengifoc@eafit.edu.co>
2025-04-06 11:11:30 -0500

    logs

---
+++ abc4sfa/2a_empirical_applications_sa/jobs/job_app_name=indonesianRice_time_limit_minutes=1800.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --partition=longjobs
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=07-00:00:00
+#SBATCH --job-name=job_app_name=indonesianRice_time_limit_minutes=1800
+#SBATCH --output=abc4sfa/2a_empirical_applications_sa/apolo_info/job_app_name=indonesianRice_time_limit_minutes=1800.out
+#SBATCH --error=abc4sfa/2a_empirical_applications_sa/apolo_info/job_app_name=indonesianRice_time_limit_minutes=1800.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=mmanzurg@eafit.edu.co
+
+echo "Tue Apr  1 23:04:41 -05 2025: Cargando módulo python/3.6.0_miniconda-4.3.11_gcc-11.2.0"
+module load python/3.6.0_miniconda-4.3.11_gcc-11.2.0
+echo "Tue Apr  1 23:04:41 -05 2025: activando venv abc4sfa"
+source activate abc4sfa
+
+echo "Tue Apr  1 23:04:41 -05 2025: Ejecutando Rscript abc4sfa/2a_empirical_applications_sa/jobs/main_app_name=indonesianRice_time_limit_minutes=1800.R"
+Rscript abc4sfa/2a_empirical_applications_sa/jobs/main_app_name=indonesianRice_time_limit_minutes=1800.R
