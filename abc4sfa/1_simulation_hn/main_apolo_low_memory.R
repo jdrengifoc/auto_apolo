62fc0b5a04eba43c00fa22034591217f34ae9cf6
Juan David Rengifo Castro <jdrengifoc@eafit.edu.co>
2025-04-06 12:49:37 -0500

    Exp miss specification hn

--- abc4sfa/1_simulation_hn/main_apolo_low_memory.R
+++ abc4sfa/1_simulation_hn/main_apolo_low_memory.R
@@ -7,23 +7,24 @@ source(file.path(SETUP_FOLDER, 'functions.R'))
 
 FOLDER <- 'abc4sfa/1_simulation_hn_resources'
 # Read inputs.
-ABC_inputs <- readRDS(file.path(FOLDER, 'Data/Inputs/BK_Ystats.RData'))
-# ABC_inputs <- readRDS(file.path(FOLDER, 'Data/Inputs/BK_exp_Ystats.RData'))
+# ABC_inputs <- readRDS(file.path(FOLDER, 'Data/Inputs/BK_Ystats.RData'))
+ABC_inputs <- readRDS(file.path(FOLDER, 'Data/Inputs/BK_exp_Ystats.RData'))
 
 # Parameters.
 ID <- "__id__"
-theta <- list(S = 1e7,                # Number of priors.
-              delta = 1000 / 1e7,      # Selected priors proportion.
-              parallel = T,           # Parallel computation?
-              batch = T               # Batch computation?
-)
+theta <- list(
+    S = 1e7,                # Number of priors.
+    delta = 1000 / 1e7,      # Selected priors proportion.
+    parallel = T,           # Parallel computation?
+    batch = T               # Batch computation?
+    )
 theta <- c(ABC_inputs[[ID]]$params, theta)
 theta$nsim <- 100
-theta$model <- 'exp'
+theta$model <- 'hn'
 
 filename <- file.path(
     FOLDER, 
-    sprintf("Data/Outputs/BK_hn_%s_misspecification_exp_%s.RData", theta$ID, Sys.Date())
+    sprintf("Data/Outputs/BK_exp_%s_misspecification_hn_%s.RData", theta$ID, Sys.Date())
     )
 # filename <- file.path(FOLDER, sprintf("Data/Outputs/BK_exp_%s.RData", theta$ID))
 
