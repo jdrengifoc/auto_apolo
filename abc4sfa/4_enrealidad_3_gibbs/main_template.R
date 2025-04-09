29ef8464d74ccdc980864175e0fd26f7ded318f3
Juan - Apolo <jdrengifoc@eafit.edu.co>
2025-04-08 19:46:21 -0500

    Arjona

--- abc4sfa/4_enrealidad_3_gibbs/main_template.R
+++ abc4sfa/4_enrealidad_3_gibbs/main_template.R
@@ -15,7 +15,7 @@ scenario <- "__scenario__"
 
 output_file <- file.path(
   FOLDER_OUTPUT,
-  sprintf("Gibs_experiment_%s_%s_%s.RData", distribution, scenario, Sys.Date())
+  sprintf("Gibbs_experiment_%s_%s_%s.RData", distribution, scenario, Sys.Date())
 )
 
 
@@ -45,11 +45,11 @@ sims <- names(ABC_sample[[scenario]])
 sims <- sims[grepl('sim', sims)]
 
 
-for (sim in sims[1]) {
+for (sim in sims) {
   data <- c(data_base, list(y = ABC_sample[[scenario]][[sim]]$y))
   postChain <- get_posterior_time_limit(
     data, model = "production", burnin_rate=0.3, time_limit_seconds = __time_limit_seconds__,
-    max_na_iterations = 5, fixed_beta = TRUE, thinning = 1
+    max_na_iterations = 5, fixed_beta = TRUE, thinning = 10
     )
   results[[sim]][['postChain']] <- postChain
   # Save results.
